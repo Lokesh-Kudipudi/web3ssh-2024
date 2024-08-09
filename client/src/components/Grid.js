4d0dda378103fd25d5e2318780dc221939cf5de9
Lokesh Kudipudi <lokeshkudipudi2006@gmail.com>
2024-08-09 14:24:32 +0530

    Update Grid.js

--- client/src/components/Grid.js
+++ client/src/components/Grid.js
@@ -1,11 +1,9 @@
-// src/App.js
 import React from "react";
 import Card from "./Card";
-import styles from "./Grid.module.css";
 
 const Grid = ({ images }) => {
   return (
-    <div className={styles.Grid}>
+    <div>
       <div className="card-grid">
         {images.map((url, index) => (
           <Card key={index} imageUrl={url} />
